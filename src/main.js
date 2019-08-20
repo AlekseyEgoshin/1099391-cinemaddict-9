b7fd8c8648ac2f6174a27d40727e795f34ced284
Aleksey <hl-hanter1997@yandex.ru>
2019-08-21 00:37:43 +0500

    Исправления 2

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-const Сard = {
+const Card = {
   DEFAULT: 5,
   EXTRA: 2,
 };
 
 const render = (container, template, place) => container.insertAdjacentHTML(place, template);
 
-import {createSearchLine} from "./components/search-line";
-import {createUserRank} from "./components/user-rank";
+import {createSearchLine} from './components/search-line';
+import {createUserRank} from './components/user-rank';
 
-import {createMenu} from "./components/menu";
-import {createSortingLing} from "./components/sorting-line";
-import {createContent} from "./components/content";
-import {createFilmCard} from "./components/film-card";
+import {createMenu} from './components/menu';
+import {createSortingLing} from './components/sorting-line';
+import {createContent} from './components/content';
+import {createFilmCard} from './components/film-card';
 
-import {createFilmPopup} from "./components/film-popup";
+import {createFilmPopup} from './components/film-popup';
 
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
@@ -27,14 +27,14 @@ render(main, createContent(), `beforeend`);
 
 const filmListDefault = document.querySelector(`.films-list`);
 const filmContainer = filmListDefault.querySelector(`.films-list__container`);
-for (let i = 0; i < Сard.DEFAULT; i++) {
+for (let i = 0; i < Card.DEFAULT; i++) {
   render(filmContainer, createFilmCard(), `beforeend`);
 }
 
 const filmListExtra = document.querySelectorAll(`.films-list--extra`);
 for (const extraList of filmListExtra) {
   const extraListContainer = extraList.querySelector(`.films-list__container`);
-  for (let i = 0; i < Сard.EXTRA; i++) {
+  for (let i = 0; i < Card.EXTRA; i++) {
     render(extraListContainer, createFilmCard(), `beforeend`);
   }
 }
