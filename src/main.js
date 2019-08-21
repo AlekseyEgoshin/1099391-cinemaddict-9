f792266e361da6f893539608abe1d7131b24cc37
Aleksey <hl-hanter1997@yandex.ru>
2019-08-21 13:21:47 +0500

    Исправлен код под критерий Б14

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-const Card = {
-  DEFAULT: 5,
-  EXTRA: 2,
-};
-
-const render = (container, template, place) => container.insertAdjacentHTML(place, template);
-
 import {createSearchLine} from './components/search-line';
 import {createUserRank} from './components/user-rank';
 
@@ -15,8 +8,16 @@ import {createFilmCard} from './components/film-card';
 
 import {createFilmPopup} from './components/film-popup';
 
+const Card = {
+  DEFAULT: 5,
+  EXTRA: 2,
+};
+
+const render = (container, template, place) => container.insertAdjacentHTML(place, template);
+
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
+const footer = document.querySelector(`.footer`);
 
 render(header, createSearchLine(), `beforeend`);
 render(header, createUserRank(), `beforeend`);
@@ -25,13 +26,17 @@ render(main, createMenu(), `beforeend`);
 render(main, createSortingLing(), `beforeend`);
 render(main, createContent(), `beforeend`);
 
+// Появляется возможность объявить переменные после отрисовки createContent
 const filmListDefault = document.querySelector(`.films-list`);
 const filmContainer = filmListDefault.querySelector(`.films-list__container`);
+
 for (let i = 0; i < Card.DEFAULT; i++) {
   render(filmContainer, createFilmCard(), `beforeend`);
 }
 
+// Появляется возможность объявить переменные после отрисовки карточек фильмов
 const filmListExtra = document.querySelectorAll(`.films-list--extra`);
+
 for (const extraList of filmListExtra) {
   const extraListContainer = extraList.querySelector(`.films-list__container`);
   for (let i = 0; i < Card.EXTRA; i++) {
@@ -39,8 +44,8 @@ for (const extraList of filmListExtra) {
   }
 }
 
-const footer = document.querySelector(`.footer`);
 render(footer, createFilmPopup(), `afterend`);
 
+// Появляется возможность объявить переменные после отрисовки Попапа для фильмов
 const filmDetail = document.querySelector(`.film-details`);
 filmDetail.classList.add(`visually-hidden`);
