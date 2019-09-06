695ed6d2cc7aa04e8c53cd7c86b6dcf18b43b9d6
Aleksey <hl-hanter1997@yandex.ru>
2019-09-07 01:53:55 +0500

    Исправления

--- src/components/film-card.js
+++ src/components/film-card.js
@@ -13,13 +13,19 @@ export const createFilmCard = ({movie, rating, year, duration, genre, descriptio
     <form class="film-card__controls">
       <button 
         class="film-card__controls-item button film-card__controls-item--add-to-watchlist${isAdded ? ` film-card__controls-item--active` : ``}"
-      >Add to watchlist</button>
+      >
+        Add to watchlist
+      </button>
       <button
         class="film-card__controls-item button film-card__controls-item--mark-as-watched${wasWatched ? ` film-card__controls-item--active` : ``}"
-      >Mark as watched</button>
+      >
+        Mark as watched
+      </button>
       <button
         class="film-card__controls-item button film-card__controls-item--favorite${isFavorite ? ` film-card__controls-item--active` : ``}"
-      >Mark as favorite</button>
+      >
+        Mark as favorite
+      </button>
     </form>
   </article>
 `;
