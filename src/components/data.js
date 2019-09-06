99ef1be8cbcdbc424476c3a054283351c5eec2c7
Aleksey <hl-hanter1997@yandex.ru>
2019-09-07 00:40:29 +0500

    Задание 1, модуль 3

---
+++ src/components/data.js
@@ -0,0 +1,116 @@
+export const getFilmInfo = () => ({
+  movie: createFilm(),
+  rating: Math.ceil((Math.random()) * 100) / 10,
+  year: [
+    `1990`,
+    `1997`,
+    `1975`,
+    `2002`,
+    `1989`,
+    `1988`,
+    `1971`,
+    `2007`,
+    `2004`,
+  ][Math.floor(Math.random() * 7)],
+  // Предположим, что длительность дается в минутах
+  duration: [
+    110,
+    150,
+    90,
+    30,
+    105,
+    132,
+    155,
+    198,
+    47,
+  ][Math.floor(Math.random() * 7)],
+  genre: `musical`,
+  description: [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra.`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+    `Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`,
+  ][Math.floor(Math.random() * 3)],
+  commentary: createComments(),
+  isAdded: Boolean(Math.round(Math.random())),
+  wasWatched: Boolean(Math.round(Math.random())),
+  isFavorite: Boolean(Math.round(Math.random())),
+});
+
+export const getFilters = () => (
+  [
+    {
+      title: `All movies`,
+      count: 0,
+    },
+    {
+      title: `Watchlist`,
+      count: 1,
+    },
+    {
+      title: `History`,
+      count: 2,
+    },
+    {
+      title: `Favorites`,
+      count: 3,
+    },
+    {
+      title: `Stats`,
+      count: 0,
+    }
+  ]
+);
+
+const String = {
+  MAX: 10,
+  MIN: 0,
+};
+
+const getRandomArbitrary = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+
+const createFilm = () => {
+  const title = [
+    `The Dance of Life`,
+    `Sagebrush Trail`,
+    `The Man with the Golden Arm`,
+    `Santa Claus Conquers the Martians`,
+    `Popeye the Sailor Meets Sindbad the Sailor`,
+    `The Great Flamarion`,
+    `Made for Each Other`,
+  ];
+
+  const poster = [
+    `the-dance-of-life.jpg`,
+    `sagebrush-trail.jpg`,
+    `the-man-with-the-golden-arm.jpg`,
+    `santa-claus-conquers-the-martians.jpg`,
+    `popeye-meets-sinbad.png`,
+    `the-great-flamarion.jpg`,
+    `made-for-each-other.png`,
+  ];
+  const index = Math.floor(getRandomArbitrary(0, title.length));
+
+  return {title: title[index], src: poster[index]};
+};
+
+const createComments = () => {
+  const comments = [
+    `Interesting setting and a good cast`,
+    `Booooooooooring`,
+    `Very very old. Meh`,
+    `Almost two hours? Seriously?`,
+    `I think it is the best film`,
+    `Sad story`,
+  ];
+
+  const count = Math.floor(getRandomArbitrary(String.MIN, String.MAX));
+  const commentaries = new Set();
+
+  for (let i = 0; i < count; i++) {
+    commentaries.add(comments[Math.floor(Math.random() * comments.length)]);
+  }
+
+  return commentaries;
+};
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import {getFilters} from './components/data';
+import {getFilmInfo} from './components/data';
+
 import {createSearchLine} from './components/search-line';
 import {createUserRank} from './components/user-rank';
 
@@ -9,42 +12,108 @@ import {createFilmCard} from './components/film-card';
 import {createFilmPopup} from './components/film-popup';
 
 const Card = {
-  DEFAULT: 5,
+  DEFAULT: 10,
   EXTRA: 2,
 };
 
-const render = (container, template, place) => container.insertAdjacentHTML(place, template);
+const renderCard = (cb, count) => {
+  const onMouseClick = () => {
+    const filmCards = document.querySelectorAll(`.film-card`);
+    const loadButton = document.querySelector(`.films-list__show-more`);
+
+    filmCards.forEach(function (singleCard) {
+      if (singleCard.classList.contains(`visually-hidden`)) {
+        singleCard.classList.remove(`visually-hidden`);
+      }
+    });
+
+    loadButton.classList.add(`visually-hidden`);
+  };
+
+  const changeStatisticsPoint = (filmCard) => {
+    const all = document.querySelector(`.main-navigation__all-movies`);
+    const watchlist = document.querySelector(`.main-navigation__watchlist`);
+    const history = document.querySelector(`.main-navigation__history`);
+    const favorites = document.querySelector(`.main-navigation__favorites`);
+    // const stats = document.querySelector(`.main-navigation__stats`);
+
+    // Обновляем значение фильтра all
+    all.textContent = parseFloat(all.textContent) + 1;
+
+    // Обновляем значение фильтра repeating
+    if (filmCard.isAdded) {
+      watchlist.textContent = parseFloat(watchlist.textContent) + 1;
+    }
+
+    // Обновляем значение фильтра history
+    if (filmCard.wasWatched) {
+      favorites.textContent = parseFloat(history.textContent) + 1;
+    }
+
+    // Обновляем значение фильтра favorites
+    if (filmCard.isFavorite) {
+      favorites.textContent = parseFloat(favorites.textContent) + 1;
+    }
+  };
+
+  const func = createFilmCard;
+
+  if (count === Card.DEFAULT) {
+    const filmList = document.querySelector(`.films-list`);
+    const filmContainer = filmList.querySelector(`.films-list__container`);
+
+    for (let i = 0; i < count; i++) {
+      const filmCard = cb();
+      changeStatisticsPoint(filmCard);
+
+      renderFilm(filmContainer, filmCard, func);
+      if (i >= Card.DEFAULT / 2) {
+        const lastAddedFilm = filmContainer.lastElementChild;
+        lastAddedFilm.classList.add(`visually-hidden`);
+      }
+    }
+
+    const loadButton = document.querySelector(`.films-list__show-more`);
+    loadButton.addEventListener(`click`, onMouseClick);
+  } else {
+    const filmList = document.querySelectorAll(`.films-list--extra`);
+
+    for (let i = 0; i < count; i++) {
+      for (const extraList of filmList) {
+        const extraListContainer = extraList.querySelector(`.films-list__container`);
+        const filmCard = cb();
+        changeStatisticsPoint(filmCard);
+
+        renderFilm(extraListContainer, filmCard, func);
+      }
+    }
+  }
+};
+
+const render = (container, template) => container.insertAdjacentHTML(`beforeend`, template);
+
+const renderFilm = (container, data, func) => {
+  container.insertAdjacentHTML(`beforeend`, new Array(1)
+    .fill(``)
+    .map(() => data)
+    .map(func)
+    .join(``));
+};
 
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
 const footer = document.querySelector(`.footer`);
 
-render(header, createSearchLine(), `beforeend`);
-render(header, createUserRank(), `beforeend`);
+render(header, createSearchLine());
+render(header, createUserRank());
+render(main, createMenu(getFilters()));
+render(main, createSortingLing());
+render(main, createContent());
 
-render(main, createMenu(), `beforeend`);
-render(main, createSortingLing(), `beforeend`);
-render(main, createContent(), `beforeend`);
+renderCard(getFilmInfo, Card.DEFAULT);
+renderCard(getFilmInfo, Card.EXTRA);
 
-// Появляется возможность объявить переменные после отрисовки createContent
-const filmListDefault = document.querySelector(`.films-list`);
-const filmContainer = filmListDefault.querySelector(`.films-list__container`);
-
-for (let i = 0; i < Card.DEFAULT; i++) {
-  render(filmContainer, createFilmCard(), `beforeend`);
-}
-
-// Появляется возможность объявить переменные после отрисовки карточек фильмов
-const filmListExtra = document.querySelectorAll(`.films-list--extra`);
-
-for (const extraList of filmListExtra) {
-  const extraListContainer = extraList.querySelector(`.films-list__container`);
-  for (let i = 0; i < Card.EXTRA; i++) {
-    render(extraListContainer, createFilmCard(), `beforeend`);
-  }
-}
-
-render(footer, createFilmPopup(), `afterend`);
+render(footer, createFilmPopup());
 
 // Появляется возможность объявить переменные после отрисовки Попапа для фильмов
 const filmDetail = document.querySelector(`.film-details`);
